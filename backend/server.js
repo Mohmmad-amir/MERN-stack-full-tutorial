07bd331f706dc4e3daa932f108d0f9bc7374a79e
Mohmmad Amir <76723565+Mohmmad-amir@users.noreply.github.com>
2023-03-18 19:24:48 +0600

    chapter 3 mongodb setup is complete

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const path = require('path')
-const { logger } = require('./middleware/logger');
+const { logger, logEvent } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3500
 
+console.log(process.env.NODE_ENV);
+
+connectDB()
 
 app.use(logger)
 app.use(cors(corsOptions))
@@ -38,4 +44,14 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once('open', () => {
+    console.log('connected to Mongodb');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+
+
+
+mongoose.connection.on('error', err => {
+    console.log(err);
+    logEvent(`${err.no}:${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})

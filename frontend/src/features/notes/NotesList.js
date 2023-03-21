4bc40e296ec1792c71692642023b682ee1faf481
Mohmmad Amir <76723565+Mohmmad-amir@users.noreply.github.com>
2023-03-21 19:27:40 +0600

    chapter 5 reactJS || complete project basic syntax

---
+++ frontend/src/features/notes/NotesList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotesList = () => {
+    return <h1>notesList</h1>;
+};
+
+export default NotesList;
---
+++ frontend/src/features/users/UserList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserList = () => {
+    return <h1>userList</h1>;
+};
+
+export default UserList;
